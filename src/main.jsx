547a296478c8f543421bdb9dfaf856a5eff937c6
Sentarocompany <lojasentaro@gmail.com>
2025-04-16 09:56:50 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './LoginPage';
-import Painel from './Painel';
+import Painel from './Painel.jsx';
 import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/painel" element={<Painel />} />
-      </Routes>
-    </BrowserRouter>
+    <Painel />
   </React.StrictMode>
 );
