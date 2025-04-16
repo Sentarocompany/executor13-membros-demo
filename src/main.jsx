8779dda374e491e556fbcad893117ff01f1e7c11
Sentarocompany <lojasentaro@gmail.com>
2025-04-16 06:29:20 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./LoginPage";
 import PainelConteudo from "./PainelConteudo";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/conteudos" element={<PainelConteudo />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/painel" element={<PainelConteudo />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
